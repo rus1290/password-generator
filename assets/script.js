6f56cd81f183d9059163cc91dc507f4b16bf89d4
Rus Zakirov <81056953+rus1290@users.noreply.github.com>
2023-03-22 15:02:15 -0400

    changed link tags of html and css referencing  to files locations

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,18 @@ function writePassword() {
 
 }
 
+var passwordLength = prompt("Please create a password between 8 and 128 characters long.")
+  if (passwordLength > 128 || passwordLength < 8) {
+    alert("Sorry, your password is either too long or too short :[")
+    return
+  }
+  var charType = confirm("Do you want lowercase characters?")
+
+function generatePassword() {
+  var charSet = " ";
+  var chartypeLower
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
